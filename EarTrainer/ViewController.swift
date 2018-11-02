7de0aa111358320470bd74a64461e8343abb8d7e
Preston Baxter <131111@s020rmc148-23.local>
2018-11-02 11:09:13 -0500

    Initial Commit

---
+++ EarTrainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EarTrainer
+//
+//  Created by Baxter, Preston L on 11/2/18.
+//  Copyright © 2018 Baxter, Preston L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
