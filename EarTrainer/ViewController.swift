b3c7777a0eda4ad81f6cbc258601b75c79d41a16
Preston Baxter <131111@s020rmc148-23.local>
2018-11-08 12:02:10 -0600

    Init?

--- EarTrainer/ViewController.swift
+++ EarTrainer/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import Beethoven
+import Pitchy
+import AudioKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var noteLabel: UILabel!
+    @IBOutlet weak var offsetLabel: UILabel!
+    @IBOutlet weak var frequencyLabel: UILabel!
+    @IBOutlet var mainView: UIView!
+    
+    var basePitch = 440
+    var tolerance = 5
+    
+    var oscillator: AKOscilator
+    
+    lazy var pitchEngine: PitchEngine = { [weak self] in
+        let config = Config(estimationStrategy: .yin)
+        let pitchEngine = PitchEngine(config: config, delegate: self)
+        pitchEngine.levelThreshold = -30.0
+        return pitchEngine
+        }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //TODO: impement settings
+        basePitch = 440
+        tolerance = 5
+        
+        oscillator = AKOscillator()
+        
+        pitchEngine.start()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func decimalFormat(_ precision: Int, _ double: Double) -> String{
+        return String(format: "%.\(precision)f", double)
+    }
 
 
+}
+
+extension ViewController: PitchEngineDelegate {
+    func pitchEngine(_ pitchEngine: PitchEngine, didReceivePitch pitch: Pitch){
+        noteLabel.text = "\(pitch.note.letter)"
+        offsetLabel.text = decimalFormat(2, pitch.closestOffset.cents)
+        frequencyLabel.text = decimalFormat(2, pitch.frequency)
+    }
+    
+    func pitchEngine(_ pitchEngine: PitchEngine, didReceiveError error: Error) {
+        print(error)
+    }
+    
+    public func pitchEngineWentBelowLevelThreshold(_ pitchEngine: PitchEngine){
+        
+    }
 }
 
